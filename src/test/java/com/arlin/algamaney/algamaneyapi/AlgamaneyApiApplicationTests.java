bd3a8d16e3dc406bce35b31e6292265001656b89
Arlin-Mafra <arlinsantana@gmail.com>
2022-09-30 21:28:20 -0300

    Estrutura do projeto

---
+++ src/test/java/com/arlin/algamaney/algamaneyapi/AlgamaneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arlin.algamaney.algamaneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgamaneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
